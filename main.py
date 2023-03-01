3b221b3c22c22b24193a2cd355acc3def6870211
Firdavs0636 <65146478+Firdavs0636@users.noreply.github.com>
2023-03-01 19:41:16 +0900

    new

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    with open("passwords.txt", mode='a') as file:
+        file.write(website_input.get())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# ---------------------------- UI SETUP ------------------------------- #
+# Window Setup
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# Canvas, Photo Setup
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+image1 = PhotoImage(file='/Users/admin/PycharmProjects/TKinter_Password_Manager/Pics/logo.png')
+canvas.create_image(100, 100, image=image1)
+canvas.grid(column=1, row=0)
+
+# Label Setup
+website_label = Label(text='Website:')
+website_label.grid(column=0, row=1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+email_username_label = Label(text='Email/Username:')
+email_username_label.grid(column=0, row=2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+password_label = Label(text='Password:')
+password_label.grid(column=0, row=3)
+
+
+# Input Entry
+website_input = Entry(width=39)
+website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
+
+
+email_username_input = Entry(width=39)
+email_username_input.grid(column=1, row=2, columnspan=2)
+email_username_input.insert(END, 'example@mail.com')
+
+
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
+
+
+# Buttons
+generate_bt = Button(text='Generate Password')
+generate_bt.grid(column=2, row=3)
+
+
+add_bt = Button(text='Add', width=37)
+add_bt.grid(column=1, row=4, columnspan=2)
+
+
+
+
+
+
+window.mainloop()
