976070b4c661ebd58a9e29744e2382a6e5778567
Firdavs <65146478+Firdavs0636@users.noreply.github.com>
2023-03-11 20:21:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
     password_list = []
@@ -83,7 +85,7 @@ window.config(padx=50, pady=50)
 
 # Canvas, Photo Setup
 canvas = Canvas(width=200, height=200, highlightthickness=0)
-image1 = PhotoImage(file='/home/friend/PycharmProjects/pythonProject/Pics/logo.png')
+image1 = PhotoImage(file='./Pics/logo.png')
 canvas.create_image(100, 100, image=image1)
 canvas.grid(column=1, row=0)
 
